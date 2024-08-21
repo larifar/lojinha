7e436298801ef4e049f3a353459f1f045d34d729
Larissa Faria <lari.f4ria@gmail.com>
2024-08-21 18:44:36 -0300

    added to model User: access list

--- src/main/java/com/larissa/virtual/lojinha/model/User.java
+++ src/main/java/com/larissa/virtual/lojinha/model/User.java
@@ -1,15 +1,18 @@
 package com.larissa.virtual.lojinha.model;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Table(name = "users")
-public class User implements Serializable {
+public class User implements Serializable, UserDetails {
 
     @Serial
     private static final long serialVersionUID = 1L;
@@ -21,6 +24,17 @@ public class User implements Serializable {
     private String name;
     private String email;
     private String CPF;
+
+    private String password;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "users_access",
+            uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "access_id"}, name = "unique_user_access"),
+    joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id", table = "user",
+            foreignKey = @ForeignKey(name = "user_fk", value = ConstraintMode.CONSTRAINT)),
+    inverseJoinColumns = @JoinColumn(name = "access_id", referencedColumnName = "id", table = "access",
+            foreignKey = @ForeignKey(name = "access_fk", value = ConstraintMode.CONSTRAINT)))
+    private List<Access> accesses;
 
     @OneToMany(mappedBy = "user", orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Address> addresses = new ArrayList<Address>();
@@ -39,6 +53,10 @@ public class User implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getEmail() {
@@ -78,5 +96,20 @@ public class User implements Serializable {
     @Override
     public int hashCode() {
         return (int) (id ^ (id >>> 32));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.accesses;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
     }
 }
