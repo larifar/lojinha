f3027926f1476fedd1512c7401d02fd9f090a07a
Larissa Faria <lari.f4ria@gmail.com>
2024-08-21 15:49:52 -0300

    created model: Access

---
+++ src/main/java/com/larissa/virtual/lojinha/model/Access.java
@@ -0,0 +1,54 @@
+package com.larissa.virtual.lojinha.model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "access")
+public class Access implements GrantedAuthority, Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String description; // ROLE_ADMIN etc
+    @Override
+    public String getAuthority() {
+        return this.description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Access access = (Access) o;
+
+        return id == access.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+}
