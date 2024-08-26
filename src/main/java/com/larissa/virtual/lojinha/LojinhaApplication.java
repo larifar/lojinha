246bebce3c479b862142b9aa2da8b929539704f6
Larissa Faria <lari.f4ria@gmail.com>
2024-08-26 12:57:58 -0300

    configs application.properties

--- src/main/java/com/larissa/virtual/lojinha/LojinhaApplication.java
+++ src/main/java/com/larissa/virtual/lojinha/LojinhaApplication.java
@@ -2,8 +2,16 @@ package com.larissa.virtual.lojinha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.larissa.virtual.lojinha.model")
+@ComponentScan(basePackages = {"com.*"})
+@EnableJpaRepositories(basePackages = {"com.larissa.virtual.lojinha.repository"})
+@EnableTransactionManagement
 public class LojinhaApplication {
 
 	public static void main(String[] args) {
